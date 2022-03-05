cfb675614d0718d6187617e932ad93ae34b918c4
Jerico <caicohr@go.byuh.edu>
2022-03-04 17:13:28 -1000

    Update Creating a Card View

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -11,25 +11,22 @@ struct CardView: View {
     let scrum: DailyScrum
     var body: some View {
         VStack(alignment: .leading) {
-        Text(scrum.title)
-            .font(.headline)
+            Text(scrum.title)
+                .font(.headline)
+                .accessibilityAddTraits(.isHeader)
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
-                    .accessibilityElement(children: .ignore)
-                    .accessibilityLabel(Text("Attendees"))
-                    .accessibilityValue(Text("\(scrum.attendees.count)"))
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
-                    .padding(.trailing, 20)
-                    .accessibilityElement(children: .ignore)
-                    .accessibilityLabel(Text("Meeting length"))
-                    .accessibilityValue(Text("\(scrum.lengthInMinutes) minutes"))
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
+                    .labelStyle(.trailingIcon)
             }
             .font(.caption)
         }
         .padding()
-        .foregroundColor(scrum.color.accessibleFontColor)
+        .foregroundColor(scrum.theme.accentColor)
     }
 }
 
@@ -37,7 +34,7 @@ struct CardView_Previews: PreviewProvider {
     static var scrum = DailyScrum.data[0]
     static var previews: some View {
         CardView(scrum: scrum)
-            .background(scrum.color)
+            .background(scrum.theme.mainColor)
             .previewLayout(.fixed(width: 400, height:60))
     }
 }
--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -11,15 +11,15 @@ struct DailyScrum{
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
-    var color: Color
+    var theme: Theme
 }
 
 extension DailyScrum {
     static var data: [DailyScrum] {
         [
-            DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, color: Color("Design")),
-            DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis","Darla"], lengthInMinutes: 5, color: Color("App Dev")),
-            DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 1, color: Color("Web Dev"))
+            DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, theme: .yellow),
+            DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis","Darla"], lengthInMinutes: 5, theme: .orange),
+            DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
         ]
     }
 }
